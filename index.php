95ddec1fc035d825bea72eb4c98fe063eed36264
jeffersonchaves <jeffersong.s@hotmail.com>
2021-11-24 08:51:07 -0300

    create files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="valida_login.php" method="POST">
+    
+        <label for="field_usuario">usuário: </label>
+        <input type="text" name="field_usuario">
+        
+        <br />
+
+        <label for="field_senha">senha</label>
+        <input type="password" name="field_senha">
+        
+        <br />
+        <input type="submit">
+    
+    </form>
+    
+</body>
+</html>
